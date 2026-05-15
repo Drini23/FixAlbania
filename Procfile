0927cb7fa387bab5208f92c2989f63e3e65e637a
drini12345 <drini12345>
2026-05-15 16:29:29 +0200

    procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn albaniafix.wsgi --log-file -
--- albaniafix/settings.py
+++ albaniafix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rgx5w)%_ak=eny((==i)%z_-bmmace@5gf2+(a10lfy2o_*dvp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django_ratelimit.middleware.RatelimitMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
